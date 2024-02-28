105ce09afac9860a11162a964dbd3b08292290a6
dd86k <dd@dax.moe>
2024-02-28 11:15:51 -0500

    Format asm to spaces

--- syslore.c
+++ syslore.c
@@ -78,43 +78,43 @@ struct	regs_t
 
 void cpuid(struct regs_t *regs, int level)
 {
-	asm mov di,regs	;
-	asm DB 0x31,0xc0	; /* XOR EAX,EAX */
-	asm DB 0x31,0xc9	; /* XOR ECX,ECX */
-	asm mov ax,level	; 
-	asm DB 0x0f,0xa2	; /* CPUID */
+	asm mov di,regs;
+	asm DB 0x31,0xc0 ; /* XOR EAX,EAX */
+	asm DB 0x31,0xc9 ; /* XOR ECX,ECX */
+	asm mov ax,level ; 
+	asm DB 0x0f,0xa2 ; /* CPUID */
 	/* Move EAX */
-	asm mov word ptr [di+2],ax	; /* To lower EAX */
-	asm DB 0x66,0xc1,0xe8,0x10	; /* SHR EAX,16 */
-	asm mov word ptr [di],ax	; /* To upper EAX */
+	asm mov word ptr [di+2],ax ; /* To lower EAX */
+	asm DB 0x66,0xc1,0xe8,0x10 ; /* SHR EAX,16 */
+	asm mov word ptr [di],ax   ; /* To upper EAX */
 	/* Move EBX */
-	asm mov word ptr [di+6],bx	; /* To lower EBX */
-	asm DB 0x66,0xc1,0xeb,0x10	; /* SHR EBX,16 */
-	asm mov word ptr [di+4],bx	; /* To upper EBX */
+	asm mov word ptr [di+6],bx ; /* To lower EBX */
+	asm DB 0x66,0xc1,0xeb,0x10 ; /* SHR EBX,16 */
+	asm mov word ptr [di+4],bx ; /* To upper EBX */
 	/* Move ECX */
-	asm mov word ptr [di+10],cx	; /* To lower ECX */
-	asm DB 0x66,0xc1,0xe9,0x10	; /* SHR ECX,16 */
-	asm mov word ptr [di+8],cx	; /* To upper ECX */
+	asm mov word ptr [di+10],cx ; /* To lower ECX */
+	asm DB 0x66,0xc1,0xe9,0x10  ; /* SHR ECX,16 */
+	asm mov word ptr [di+8],cx  ; /* To upper ECX */
 	/* Move EDX */
-	asm mov word ptr [di+14],dx	; /* To lower EDX */
-	asm DB 0x66,0xc1,0xea,0x10	; /* SHR EDX,16 */
-	asm mov word ptr [di+12],dx	; /* To upper EDX */
+	asm mov word ptr [di+14],dx ; /* To lower EDX */
+	asm DB 0x66,0xc1,0xea,0x10  ; /* SHR EDX,16 */
+	asm mov word ptr [di+12],dx ; /* To upper EDX */
 }
 
 int hasfpu()
 {
 	asm fninit;
-	asm DB 0xdf,0xe0;	/* FNSTSW AX */
+	asm DB 0xdf,0xe0 ; /* FNSTSW AX */
 	return _AL == 0;
 }
 
 int hascpuid()
 {
-	asm DB 0x66,0x68,0,0,0x20,0	; /* PUSH 0x20_0000 */
-	asm DB 0x66,0x9d		; /* POPFD */
-	asm DB 0x66,0x9c		; /* PUSHFD */
-	asm DB 0x66,0x58		; /* POP EAX */
-	asm DB 0x66,0xc1,0xe8,0x15	; /* SHR EAX,21 */
+	asm DB 0x66,0x68,0,0,0x20,0  ; /* PUSH 0x20_0000 */
+	asm DB 0x66,0x9d             ; /* POPFD */
+	asm DB 0x66,0x9c             ; /* PUSHFD */
+	asm DB 0x66,0x58             ; /* POP EAX */
+	asm DB 0x66,0xc1,0xe8,0x15   ; /* SHR EAX,21 */
 	return _AL;
 }
 
@@ -125,43 +125,43 @@ int cputype()
 {
 	/* 8086 has FLAGS[15:12] always set and cannot be cleared */
 	/* 8086 also changes value of SP after pushing */
-	asm pushf	; /* Push original FLAGS */
-	asm pop ax	; /* Get FLAGS into AX */
-	asm mov cx,ax	; /* Save original FLAGS */
-	asm and ax,0xfff	; /* Clear bits 15:12 */
-	asm push ax	; /* Push new value into stack*/
-	asm popf	; /* Set FLAGS from value from stack */
-	asm pushf	; /* Get new FLAGS */
-	asm pop ax	; /* Save FLAGS value */
-	asm and ax,0xf000	; /* Keep bits 15:12 */
-	asm cmp ax,0xf000	; /* Anything in FLAGS[15:12]? */
-	asm jne check_286	; /* If cleared, it's probably an i286 */
-	asm push sp	; /* Not an i286? Check for PUSH/SP */
-	asm pop dx	; /* Save SP value */
-	asm cmp dx,sp	; /* If current SP value */
-	asm jne case_8086	; 
+	asm pushf         ; /* Push original FLAGS */
+	asm pop ax        ; /* Get FLAGS into AX */
+	asm mov cx,ax     ; /* Save original FLAGS */
+	asm and ax,0xfff  ; /* Clear bits 15:12 */
+	asm push ax       ; /* Push new value into stack*/
+	asm popf          ; /* Set FLAGS from value from stack */
+	asm pushf         ; /* Get new FLAGS */
+	asm pop ax        ; /* Save FLAGS value */
+	asm and ax,0xf000 ; /* Keep bits 15:12 */
+	asm cmp ax,0xf000 ; /* Anything in FLAGS[15:12]? */
+	asm jne check_286 ; /* If cleared, it's probably an i286 */
+	asm push sp       ; /* Not an i286? Check for PUSH/SP */
+	asm pop dx        ; /* Save SP value */
+	asm cmp dx,sp     ; /* If current SP value */
+	asm jne case_8086; 
 	/*asm jmp case_unknown	;*/
 check_286: /* 80286 has FLAGS[15:12] always cleared and cannot be set */
-	asm or cx,0xf000	; /* Try to set bits 15:12 to saved FLAGS */
-	asm push cx	;
-	asm popf	; /* Sets FLAGS from CX */
-	asm pushf	;
-	asm pop dx	; /* Get new FLAGS into DX */
-	asm and dx,0xf000	; /* Clear FLAGS[11:0] */
-	asm jz case_286	; /* Jump if FLAGS[15:12] is cleared */
+	asm or cx,0xf000  ; /* Try to set bits 15:12 to saved FLAGS */
+	asm push cx       ;
+	asm popf          ; /* Sets FLAGS from CX */
+	asm pushf;
+	asm pop dx        ; /* Get new FLAGS into DX */
+	asm and dx,0xf000 ; /* Clear FLAGS[11:0] */
+	asm jz case_286   ; /* Jump if FLAGS[15:12] is cleared */
 check_386: /* 80386 doesn't have toggable EFLAGS[AC], 80486 does */
-	asm DB 0x66,0x9c	; /* PUSHFD: Push original EFLAGS */
-	asm DB 0x66,0x58	; /* POP EAX: Get ELFAGS */
-	asm DB 0x66,0x89,0xc1	; /* MOV ECX,EAX: Save EFLAGS */
-	asm DB 0x66,0x35,0,0,4,0; /* XOR EAX,0x40000: Flip EFLAGS[AC] */
-	asm DB 0x66,0x50	; /* PUSH EAX: Save new value on stack */
-	asm DB 0x66,0x9d	; /* POPFD: Replace EFLAGS */
-	asm DB 0x66,0x9c	; /* PUSHFD: Get new EFLAGS */
-	asm DB 0x66,0x58	; /* POP EAX: Store EFLAGS */
-	asm DB 0x66,0x31,0xc8	; /* XOR EAX,ECX: If can't toggle AC, it is 80386 */
-	asm jz case_386	;
-	asm DB 0x66,0x51	; /* PUSH ECX */
-	asm DB 0x66,0x9d	; /* POPFD: Restore EFLAGS */
+	asm DB 0x66,0x9c         ; /* PUSHFD: Push original EFLAGS */
+	asm DB 0x66,0x58         ; /* POP EAX: Get ELFAGS */
+	asm DB 0x66,0x89,0xc1    ; /* MOV ECX,EAX: Save EFLAGS */
+	asm DB 0x66,0x35,0,0,4,0 ; /* XOR EAX,0x40000: Flip EFLAGS[AC] */
+	asm DB 0x66,0x50         ; /* PUSH EAX: Save new value on stack */
+	asm DB 0x66,0x9d         ; /* POPFD: Replace EFLAGS */
+	asm DB 0x66,0x9c         ; /* PUSHFD: Get new EFLAGS */
+	asm DB 0x66,0x58         ; /* POP EAX: Store EFLAGS */
+	asm DB 0x66,0x31,0xc8    ; /* XOR EAX,ECX: If can't toggle AC, it is 80386 */
+	asm jz case_386;
+	asm DB 0x66,0x51         ; /* PUSH ECX */
+	asm DB 0x66,0x9d         ; /* POPFD: Restore EFLAGS */
 
 case_486:
 	return CPU_i486SX;
@@ -194,35 +194,35 @@ int fputype()
 {
 	static unsigned short _fpu_cw;
 	
-	asm fninit	; /* Resets FPU if present */
-	asm DB 0xdf,0xe0	; /* FNSTSW AX: Get FPU status word */
-	asm cmp	al,0	; /* Do we have anything? */
-	asm jne	case_none	; /* No FPU then */
+	asm fninit          ; /* Resets FPU if present */
+	asm DB 0xdf,0xe0    ; /* FNSTSW AX: Get FPU status word */
+	asm cmp	al,0        ; /* Do we have anything? */
+	asm jne	Lnone        ; /* No FPU then */
 	/* Check for 8087 */
-	asm and	ax,0xff7f	; /* Clear other bits from AX */
-	asm mov	word [_fpu_cw],ax	; /* Load AX into mem */
-	asm fldcw word [_fpu_cw]	; /* Load control word into FPU */
-	asm fdisi	; /* 8087-only instruction */
-	asm fstcw word [_fpu_cw]	; /* Get control word */
-	asm test word ptr [_fpu_cw],0x80	; /* Did FDISI do anything? */
-	asm jne case_8087	; /* FDISI did something, FPU: 8087 */
+	asm and	ax,0xff7f                ; /* Clear other bits from AX */
+	asm mov	word [_fpu_cw],ax        ; /* Load AX into mem */
+	asm fldcw word [_fpu_cw]         ; /* Load control word into FPU */
+	asm fdisi                        ; /* 8087-only instruction */
+	asm fstcw word [_fpu_cw]         ; /* Get control word */
+	asm test word ptr [_fpu_cw],0x80 ; /* Did FDISI do anything? */
+	asm jne L8087	                 ; /* FDISI did something, so 8087 */
 	/* Check for +INF/-INF between 80287/80387 */
-	asm fld1		; /* Push +1.0, this will be st(0) */
-	asm fldz		; /* Push +0.0, this will be st(1) */
-	asm fdiv		; /* (fdivp st1,st0) 1.0/0.0 = +INF, then pop, TOP=st0 */
-	asm fld		st(0)	; /* Push st0 value (+INF) again into stack (now st1) */
-	asm fchs		; /* Toggle sign to st0, making st0 -INF */
-	asm fcompp		; /* See if st0/st1 are the same, then pop both */
-	asm DB 0xdf,0xe0	; /* FSTSW AX: Get status word */
-	asm sahf		; /* Save AH into low FLAGS to see if infinites matched */
-	asm jz	case_80287	; /* <= 80287: +inf == -inf */
+	asm fld1          ; /* Push +1.0, this will be st(0) */
+	asm fldz          ; /* Push +0.0, this will be st(1) */
+	asm fdiv          ; /* (fdivp st1,st0) 1.0/0.0 = +INF, then pop, TOP=st0 */
+	asm fld st(0)     ; /* Push st0 value (+INF) again into stack (now st1) */
+	asm fchs          ; /* Toggle sign to st0, making st0 -INF */
+	asm fcompp        ; /* See if st0/st1 are the same, then pop both */
+	asm DB 0xdf,0xe0  ; /* FSTSW AX: Get status word */
+	asm sahf          ; /* Save AH into low FLAGS to see if infinites matched */
+	asm jz	L80287    ; /* +inf == -inf -> 80287 and earlier */
 	
 	return FPU_80387;
-case_80287:
+L80287:
 	return FPU_80287;
-case_8087:
+L8087:
 	return FPU_8087;
-case_none:
+Lnone:
 	return FPU_NONE;
 }
 
